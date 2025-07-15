fb3000607590829f865a6c13497427d44cec82f6
saurashya <saurashya1@gmail.com>
2025-07-15 15:40:21 +0545

    Setup: Initial folder structure setup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <div className='flex h-screen w-screen justify-center items-center gap-8'>
+      <img src="/assets/logo.png" height={100} width={100}/>
+      <h1 className='text-6xl'>Baker's Bakery Website Coming Soon!</h1>
+    </div>
   )
 }
 
