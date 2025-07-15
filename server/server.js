caade3ab82694e513d890cb0c3e75a08b57f949b
saurashya <saurashya1@gmail.com>
2025-07-15 22:18:39 +0545

    Setup: initial project folder setup.

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to Bakers Bakery API!')
+})
+
+// Start the server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
