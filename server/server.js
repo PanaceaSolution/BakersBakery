66c1e125cc61854c1fc1bebbfb9cbe6debce039c
Sijan-Giri <girisijan346@gmail.com>
2025-07-16 17:09:19 +0545

    Setup: Database connected successfully !!

--- server/server.js
+++ server/server.js
@@ -1,13 +1,21 @@
 import express from 'express'
-
 const app = express()
+import sql from "./src/database/index.js"
 
 // Middleware
 app.use(express.json())
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to Bakers Bakery API!')
+app.get('/',async (req, res) => {
+  try {
+  const result = await sql `SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+  console.log("Database connected successfully !!")
+} catch (error) {
+  console.log(error)
+}
 })
 
 // Start the server
