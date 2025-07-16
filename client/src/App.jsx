1f71836f1236b89600a1d291a74f603694fde4d6
Shubham Shrestha <shrestha.subham.60@gmail.com>
2025-07-16 16:03:48 +0545

    setup:initial

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Homepage';
 
 const App = () => {
   return (
-    <div className='flex h-screen w-screen justify-center items-center gap-8'>
-      <img src="/assets/logo.png" height={100} width={100}/>
-      <h1 className='text-6xl'>Baker's Bakery Website Coming Soon!</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
