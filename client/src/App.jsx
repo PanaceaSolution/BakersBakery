1f79f510d90a1d257a33c4644b960218fa7efaac
ashim <magarashim69086@gmail.com>
2025-07-17 12:16:18 +0545

    routes:homepage route created using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/Routes";
 
 const App = () => {
   return (
-    <div className='flex h-screen w-screen justify-center items-center gap-8'>
-      <img src="/assets/logo.png" height={100} width={100}/>
-      <h1 className='text-6xl'>Baker's Bakery Website Coming Soon!</h1>
-    </div>
-  )
-}
+    <>
+      {/* <div className="flex h-screen w-screen justify-center items-center gap-8">
+        <img src="/assets/logo.png" height={100} width={100} />
+        <h1 className="text-6xl">Baker's Bakery Website Coming Soon!</h1>
+      </div> */}
 
-export default App
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
